76e1e25e6503736f089f8111d55e60e06918f6a7
nathybenavids <73511871+nathybenavids@users.noreply.github.com>
2021-06-23 01:29:05 -0500

    Add files via upload

---
+++ Clase1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaConsola
+{
+    class persona
+    {
+        public string nombre;
+        public string apellido;
+        public string direccionSector;
+        public string callePrincipal;
+        public string calleSecundaria;
+        public string cid;
+        public int edad;
+        public float telefono;
+        public bool correo;
+        public bool esEstudiante;
+
+         
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ProgramaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba su nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Escriba su apellido: ");
+            string apellido = Console.ReadLine();
+
+            Console.Write("Ingrese el sector donde vive: ");
+            string direccionSector = Console.ReadLine();
+
+            Console.Write("Ingrese la calle principal de su domicilio: ");
+            string callePrincipal = Console.ReadLine();
+
+            Console.Write("Ingrese la calle secundaria de su domicilio: ");
+            string calleSecundaria = Console.ReadLine();
+
+            Console.Write("Escriba su numero de cedula: ");
+            string cid = Console.ReadLine();
+
+            Console.Write("¿Que edad tiene?: ");
+            string edad = Console.ReadLine();
+            int edad1 = int.Parse(edad);
+
+            Console.Write("Ingrese su numero de telefono: ");
+            string telefono = Console.ReadLine();
+            float telefono1 = float.Parse(telefono);
+
+            Console.Write("Escriba su correo electronico");
+            string correo = Console.ReadLine();
+            bool correo1 = correo == "@";
+
+            Console.Write("¿Usted es estudiante? Responda si/no: ");
+            string esEstudiante = Console.ReadLine().ToUpper();
+            bool esEstudiante1 = esEstudiante == "si";
+
+        }
+    }
+}
